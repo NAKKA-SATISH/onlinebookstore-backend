7006c6fa3b02605a682acb08cbd76c47ae606d27
Satish Nakka <satishnakka2003@gmail.com>
2026-06-26 19:02:06 +0530

    Add AuthController for user signup and login

---
+++ OnlineBookStore/src/main/java/com/Projects/OnlineBookStore/controller/AuthController.java
@@ -0,0 +1,43 @@
+package com.Projects.OnlineBookStore.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.Projects.OnlineBookStore.entity.User;
+import com.Projects.OnlineBookStore.repo.UserRepository;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+
+    @Autowired
+    private UserRepository repo;
+
+    @PostMapping("/signup")
+    public User signup(@RequestBody User user) {
+
+        return repo.save(user);
+
+    }
+
+    @PostMapping("/login")
+    public String login(@RequestBody User user) {
+
+        User db = repo.findByUsername(
+                user.getUsername()
+        );
+
+        if (db != null &&
+                db.getPassword().equals(
+                        user.getPassword()
+                )) {
+
+            return "SUCCESS";
+
+        }
+        return "INVALID";
+    }
+}
