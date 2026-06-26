68ce0338a703585b12a3870bd960138502be48b9
Satish Nakka <satishnakka2003@gmail.com>
2026-06-26 18:41:33 +0530

    Refactor CorsConfig for cleaner formatting

--- OnlineBookStore/src/main/java/com/Projects/OnlineBookStore/config/CorsConfig.java
+++ OnlineBookStore/src/main/java/com/Projects/OnlineBookStore/config/CorsConfig.java
@@ -7,23 +7,20 @@ import org.springframework.web.servlet.config.annotation.*;
 @Configuration
 public class CorsConfig {
 
-@Bean
-public WebMvcConfigurer corsConfigurer() {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
 
-return new WebMvcConfigurer() {
+        return new WebMvcConfigurer() {
 
-@Override
-public void addCorsMappings(CorsRegistry registry) {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
 
-registry.addMapping("/**")
-.allowedOrigins("http://localhost:3000")
-.allowedMethods("*")
-.allowedHeaders("*");
-
-}
-
-};
-
-}
-
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:3000")
+                        .allowedMethods("*")
+                        .allowedHeaders("*");
+                
+            }
+        };
+    }
 }
