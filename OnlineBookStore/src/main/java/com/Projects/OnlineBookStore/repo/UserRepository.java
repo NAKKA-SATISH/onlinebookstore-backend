04a3b6f29247488377f98ab6a6b4d432585c8560
Satish Nakka <satishnakka2003@gmail.com>
2026-06-26 19:07:42 +0530

    commit

--- OnlineBookStore/src/main/java/com/Projects/OnlineBookStore/repo/UserRepository.java
+++ OnlineBookStore/src/main/java/com/Projects/OnlineBookStore/repo/UserRepository.java
@@ -4,6 +4,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.Projects.OnlineBookStore.entity.User;
 
-public interface UserRepository extends JpaRepository<User,Long>{
+public interface UserRepository
+extends JpaRepository<User,Long>{
+
+User findByUsername(
+String username
+);
 
 }
