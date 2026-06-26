b0964c7e965da758b8e5f0ae85dde2e68a65104a
Satish Nakka <satishnakka2003@gmail.com>
2026-06-26 18:42:13 +0530

    Update SecurityConfig to permit all requests

--- OnlineBookStore/src/main/java/com/Projects/OnlineBookStore/config/SecurityConfig.java
+++ OnlineBookStore/src/main/java/com/Projects/OnlineBookStore/config/SecurityConfig.java
@@ -2,24 +2,43 @@ package com.Projects.OnlineBookStore.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
 
-@Bean
-public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    @Bean
+    public SecurityFilterChain securityFilterChain(
+            HttpSecurity http
+    ) throws Exception {
 
-    http
-        .csrf(csrf -> csrf.disable())
-        .cors(cors -> {})
-        .authorizeHttpRequests(auth -> auth
-                .requestMatchers("/books/**").permitAll()
-                .anyRequest().authenticated()
-        );
+        http
+                .csrf(csrf -> csrf.disable())
 
-    return http.build();
-}
+                .cors(cors -> {
+                })
 
+                .authorizeHttpRequests(auth -> auth
+
+                        .requestMatchers(
+                                "/auth/**"
+                        ).permitAll()
+
+                        .requestMatchers(
+                                "/books/**"
+                        ).permitAll()
+
+                        .anyRequest()
+                        .permitAll()
+
+                )
+
+                .formLogin(form -> form.disable())
+
+                .httpBasic(basic -> basic.disable());
+
+        return http.build();
+    }
 }
