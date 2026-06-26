2fa367ee52e79395fec6d780b9782c21df94fb32
Satish Nakka <satishnakka2003@gmail.com>
2026-06-26 19:05:22 +0530

    commit

--- OnlineBookStore/src/main/java/com/Projects/OnlineBookStore/entity/User.java
+++ OnlineBookStore/src/main/java/com/Projects/OnlineBookStore/entity/User.java
@@ -1,21 +1,42 @@
 package com.Projects.OnlineBookStore.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import lombok.Data;
+import jakarta.persistence.*;
 
 @Entity
-@Data
+@Table(name="users")
+
 public class User {
 
 @Id
-@GeneratedValue(strategy = GenerationType.IDENTITY)
+@GeneratedValue(strategy =
+GenerationType.IDENTITY)
+
 private Long id;
 
 private String username;
+
 private String password;
-private String role;
+
+public User(){}
+
+public Long getId() {
+return id;
+}
+
+public String getUsername() {
+return username;
+}
+
+public void setUsername(String username) {
+this.username=username;
+}
+
+public String getPassword() {
+return password;
+}
+
+public void setPassword(String password) {
+this.password=password;
+}
 
 }
