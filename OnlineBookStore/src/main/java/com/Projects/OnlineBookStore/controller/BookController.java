98dc5907b717eee1fe18b248e96738b4c56e9a76
Satish Nakka <satishnakka2003@gmail.com>
2026-06-26 18:43:18 +0530

    Enhance updateBook to include image and category
    Added image and category fields to the updateBook method.

--- OnlineBookStore/src/main/java/com/Projects/OnlineBookStore/controller/BookController.java
+++ OnlineBookStore/src/main/java/com/Projects/OnlineBookStore/controller/BookController.java
@@ -1,7 +1,6 @@
 package com.Projects.OnlineBookStore.controller;
 
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -21,30 +20,40 @@ import com.Projects.OnlineBookStore.repo.BookRepository;
 @RequestMapping("/books")
 public class BookController {
 
-    @Autowired
-    private BookRepository repo;
+	@Autowired
+	private BookRepository repo;
 
-    @GetMapping
-    public List<Book> getBooks(){
-        return repo.findAll();
-    }
+	@GetMapping
+	public List<Book> getBooks() {
+		return repo.findAll();
+	}
 
-    @PostMapping
-    public Book addBook(@RequestBody Book book){
-        return repo.save(book);
-    }
+	@PostMapping
+	public Book addBook(@RequestBody Book book) {
+		return repo.save(book);
+	}
 
-    @DeleteMapping("/{id}")
-    public void deleteBook(@PathVariable Long id){
-        repo.deleteById(id);
-    }
+	@DeleteMapping("/{id}")
+	public void deleteBook(@PathVariable Long id) {
+		repo.deleteById(id);
+	}
 
-    @PutMapping("/{id}")
-    public Book updateBook(@PathVariable Long id, @RequestBody Book book){
-        Book b = repo.findById(id).get();
-        b.setTitle(book.getTitle());
-        b.setAuthor(book.getAuthor());
-        b.setPrice(book.getPrice());
-        return repo.save(b);
-    }
+	@PutMapping("/{id}")
+	public Book updateBook(@PathVariable Long id, @RequestBody Book book) {
+
+		Book b = repo.findById(id).get();
+
+		b.setTitle(book.getTitle());
+
+		b.setAuthor(book.getAuthor());
+
+		b.setPrice(book.getPrice());
+
+		b.setImage(book.getImage());
+
+		b.setCategory(book.getCategory());
+
+		return repo.save(b);
+
+	}
 }
