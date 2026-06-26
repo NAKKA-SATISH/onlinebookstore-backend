ea003404b860c729e1aa638ddeae0885e11c450e
Satish Nakka <satishnakka2003@gmail.com>
2026-06-26 19:03:24 +0530

    commit

--- OnlineBookStore/src/main/java/com/Projects/OnlineBookStore/entity/Book.java
+++ OnlineBookStore/src/main/java/com/Projects/OnlineBookStore/entity/Book.java
@@ -1,22 +1,77 @@
 package com.Projects.OnlineBookStore.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import lombok.Data;
+import jakarta.persistence.*;
 
 @Entity
-@Data
 public class Book {
 
 @Id
-@GeneratedValue(strategy = GenerationType.IDENTITY)
+@GeneratedValue(
+strategy =
+GenerationType.IDENTITY
+)
+
 private Long id;
 
 private String title;
+
 private String author;
+
 private double price;
-private int stock;
+
+private String image;
+
+private String category;
+
+public Book() {
+}
+
+public Long getId() {
+return id;
+}
+
+public void setId(Long id) {
+this.id = id;
+}
+
+public String getTitle() {
+return title;
+}
+
+public void setTitle(String title) {
+this.title = title;
+}
+
+public String getAuthor() {
+return author;
+}
+
+public void setAuthor(String author) {
+this.author = author;
+}
+
+public double getPrice() {
+return price;
+}
+
+public void setPrice(double price) {
+this.price = price;
+}
+
+public String getImage() {
+return image;
+}
+
+public void setImage(String image) {
+this.image = image;
+}
+
+public String getCategory() {
+return category;
+}
+
+public void setCategory(String category) {
+this.category = category;
+}
 
 }
